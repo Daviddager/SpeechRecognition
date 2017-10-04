1228e69a2d25095cbad8d8b17afa6326cfa3a678
Daviddager <drdagerm@gmail.com>
2017-10-04 09:15:35 -0500

    Changed Languaje

--- app/src/main/java/unal/edu/co/speechrecognizer/MainActivity.java
+++ app/src/main/java/unal/edu/co/speechrecognizer/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
     private void promptSpeechInput(){
         Intent intent = new Intent( RecognizerIntent.ACTION_RECOGNIZE_SPEECH );
         intent.putExtra( RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM );
-        intent.putExtra( RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault() );
+        intent.putExtra( RecognizerIntent.EXTRA_LANGUAGE, "es-CO" );
         intent.putExtra( RecognizerIntent.EXTRA_PROMPT, getString( R.string.speech_prompt ) );
 
         try{
